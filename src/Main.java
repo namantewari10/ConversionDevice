44826c802a836c97972d11aa566f637dccd344f2
Naman <namantewari10@gmail.com>
2022-07-26 14:30:33 +0530

    conversion of feet to inches

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Sample commit message");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the value in feet: ");
+        int valueInFeet = Integer.parseInt(br.readLine().trim());
+        System.out.println("valueInInches:" + valueInFeet*12);
     }
 }
